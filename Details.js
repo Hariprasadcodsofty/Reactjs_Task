f12f537d5d0cc552565558e2327e4a946d5349eb
HariprasadP <hari1408004@gmail.com>
2024-08-10 08:38:52 +0530

    Details.js

--- Details.js
+++ Details.js
@@ -3,10 +3,10 @@ export function Details()
 {
     let from = () => {
         let name=document.getElementById('name').value;
-        let age=document.getElementById('name').value;
-        let num=document.getElementById('name').value;
-        let email=document.getElementById('name').value;
-        let dob=document.getElementById('name').value;
+        let age=document.getElementById('age').value;
+        let num=document.getElementById('num').value;
+        let email=document.getElementById('email').value;
+        let dob=document.getElementById('dob').value;
         alert("Hi "+name+"your details have been registered");
     }
     return(
@@ -15,7 +15,7 @@ export function Details()
         <label>Enter your Name:</label><br></br>
         <input type="text" id="name" name="name"></input><br></br>
         <label>Enter your Age:</label><br></br>
-        <input type="texy" id="age" name="age"></input><br></br>
+        <input type="age" id="age" name="age"></input><br></br>
         <label>Enter your phone number:</label><br></br>
         <input type="tel" id="num" name="num"></input><br></br>
         <label>Enter your email address:</label><br></br>
